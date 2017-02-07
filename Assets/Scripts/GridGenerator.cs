65a126474ad6493822be2b4696b968cb1269b55f
dbga_36 <dbga_36@dbga.local>
2017-02-07 17:21:24 +0100

    - Android

---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridGenerator : MonoBehaviour
+{
+    public GameObject sprite;
+    private int[,] grid = new int[3, 3];
+    
+    private void Start()
+    {
+        GameObject parent = new GameObject("Sprite");
+        parent.transform.position = Vector3.zero;
+
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                GameObject newcell = Instantiate(sprite);
+                newcell.transform.position = new Vector3(j * 3f, -i *3);
+                newcell.name = "Cella " + i + " " + j;
+                newcell.transform.SetParent(parent.transform);
+            }
+        }
+    }
+}
