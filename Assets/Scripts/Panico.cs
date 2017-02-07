be01c26dd4e8b3838849c82609cb405e27b0f5dc
dbga_09 <dbga_09@dbga.local>
2017-02-07 22:02:39 +0100

    add menù flow and music sound

--- Assets/Scripts/Panico.cs
+++ Assets/Scripts/Panico.cs
@@ -4,8 +4,33 @@ using UnityEngine.SceneManagement;
 
 public class Panico : MonoBehaviour
 {
+    public GameObject panelMenù;
+    public GameObject panelRanks;
+    public GameObject panelCredits;
+
     public void ChangeScene()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Ranks()
+    {
+        panelMenù.SetActive(false);
+        panelRanks.SetActive(true);
+        panelCredits.SetActive(false);
+    }
+
+    public void Credits()
+    {
+        panelMenù.SetActive(false);
+        panelRanks.SetActive(false);
+        panelCredits.SetActive(true);
+    }
+
+    public void Menù()
+    {
+        panelMenù.SetActive(true);
+        panelRanks.SetActive(false);
+        panelCredits.SetActive(false);
     }
 }
