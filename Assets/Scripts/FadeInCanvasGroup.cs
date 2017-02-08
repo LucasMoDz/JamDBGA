7204f1e6b16bd8281c61c96c8c04b1482e98e902
Luca Neri <lucasmodzita@gmail.com>
2017-02-09 00:16:36 +0100

    - Fade canvas group

---
+++ Assets/Scripts/FadeInCanvasGroup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeInCanvasGroup : MonoBehaviour
+{
+    private CanvasGroup myCanvasGroup;
+
+    private void Awake()
+    {
+        myCanvasGroup = this.GetComponent<CanvasGroup>();
+    }
+
+    private IEnumerator Start()
+    {
+        while (myCanvasGroup.alpha > 0)
+        {
+            myCanvasGroup.alpha -= Time.deltaTime;
+            yield return null;
+        }
+
+        myCanvasGroup.alpha = 0;
+    }
+}
